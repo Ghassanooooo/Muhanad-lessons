79413270c9a496d99f62b61cc56a0779749a310b
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-02-13 12:14:05 +0100

    solution

---
+++ exercises-solutions/PB-language-pitfalls-solution/index.js
@@ -0,0 +1,15 @@
+// 1
+// NaN stands for Not-a-Number.
+console.log(isNaN("hello"));
+console.log(isNaN(3));
+console.log(typeof NaN);
+// Type of NaN is a number
+
+// 2
+console.log((0.1 * 0.2).toFixed(2));
+
+// 3
+console.log(Infinity / 0);
+console.log(Infinity / Infinity);
+console.log(1 / 0);
+
